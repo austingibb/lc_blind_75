552b8f55883bef9e4bd88ddb1b27c7065b5cce9f
austingibb <austingibb@gmail.com>
2026-01-15 02:19:07 -0700

    add unique paths iterative solution explanation

--- unique_paths_iterative.py
+++ unique_paths_iterative.py
@@ -1,6 +1,12 @@
 from collections import deque
 
 class Solution:
+    # Works because you always need the left and above tiles to get all unique paths the robot can take to this point
+    # left tile is always available because second for loop goes left to right
+    # top tile is always available becauase first for loop goes top to bottom (prior row is already filled when current is calculating)
+    #
+    # each tile represents the number of unique paths until that point, thus to calculate a new tiles
+    # number of unique paths the only places it can be entered from by the robot are left and above.
     def uniquePaths(self, m: int, n: int) -> int:
         f = [[0] * n for _ in range(m)]
         f[0][0] = 1
